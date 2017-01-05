47d1b9902dd169b72301d53c6222fff2c6522527
Srihari Muthuswamy <hari@Sriharis-MBP.hsd1.ca.comcast.net>
2017-01-05 15:03:31 -0800

    added httpd.yml - ansible playbook to install and configure httpd server on a centos machine

--- ansible/wrapper.sh
+++ ansible/wrapper.sh
@@ -1,3 +1,6 @@
+#######
+# runs ansible playbook to install httpd and configure httpd server in centos
+########
 yum install -y epel-release
 yum install -y ansible
 echo "localhost ansible_connection=local" >> /etc/ansible/hosts
@@ -8,7 +11,7 @@ echo "document_root: /apps/hello-http/html" > /etc/ansible/group_vars/all
 echo "Running ansible playbook to setup httpd"
 echo " "
 echo " "
-cd /etc/ansible; ansible-playbook -i hosts site.yml
+cd /etc/ansible; ansible-playbook -i hosts httpd.yml
 echo "Testing hello.html"
 echo " "
 echo " "
